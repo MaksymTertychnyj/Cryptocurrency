0ea460e1f85cadc39895038fe7729a3f415f4148
MaksymTertychnyj <maka12008@ukr.net>
2022-07-01 19:30:14 +0300

    created interface IRetrieveDataService

---
+++ Cryptocurrency/Services/Interfaces/IRetrieveDataService.cs
@@ -0,0 +1,18 @@
+﻿using Cryptocurrency.Model.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptocurrency.Services.Interfaces
+{
+    public interface IRetrieveDataService
+    {
+        Task<IEnumerable<Asset>> GetAssetsAsync(int? limit, int? offset);
+        Task<Asset> GetAssetByIdAsync(string id);
+        Task<IEnumerable<Market>> GetMarketsAsync(string? assetId, int? limit, int? offset);
+        Task<IEnumerable<Exchange>> GetExchangesAsync(int? limit, int? offset);
+        Task<Exchange> GetExchangeByIdAsync(string exchangeId);
+    }
+}
