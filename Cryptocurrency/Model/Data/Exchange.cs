f096d0572c427b005ed2253893fec50b14874f39
MaksymTertychnyj <maka12008@ukr.net>
2022-07-01 18:43:23 +0300

    created data models

---
+++ Cryptocurrency/Model/Data/Exchange.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptocurrency.Model.Data
+{
+    public class Exchange
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Rank { get; set; }
+        public string? PercentTotalVolume { get; set; }
+        public string? VolumeUsd { get; set; }
+        public string? TradingPairs { get; set; }
+        public bool? Socket { get; set; }
+        public string? ExchangeUrl { get; set; }
+        public string? Updated { get; set; }
+    }
+}
