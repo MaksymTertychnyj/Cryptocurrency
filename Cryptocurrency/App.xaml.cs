04714d9382e28359adecc9c09141ec75d9bfa60c
MaksymTertychnyj <maka12008@ukr.net>
2022-07-02 12:10:25 +0300

    configured dependency injection to use HttpClient

--- Cryptocurrency/App.xaml.cs
+++ Cryptocurrency/App.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Cryptocurrency.Services.Implementation;
+using Cryptocurrency.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Reflection;
 using System.Windows;
 
 namespace Cryptocurrency
@@ -13,5 +16,35 @@ namespace Cryptocurrency
     /// </summary>
     public partial class App : Application
     {
+        public IServiceProvider ServiceProvider { get; private set; }
+        public IConfiguration Configuration { get; private set; }
+       
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient<IRetrieveDataService, RetrieveDataService>(client =>
+            {
+                client.BaseAddress = new Uri(Configuration["docsCoincap:BaseAddress"]);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            });
+
+            services.AddSingleton(typeof(MainWindow));
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile(Path.Combine(
+                    AppDomain.CurrentDomain.BaseDirectory, "appsettings.json"), 
+                    optional: true, 
+                    reloadOnChange: false
+                 ).Build();
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            var mainWindow = ServiceProvider!.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
     }
 }
