9fe91e4443021fa2b1adffeed8e4496ec8c37ce5
MaksymTertychnyj <maka12008@ukr.net>
2022-07-05 13:26:26 +0300

    created PageService, StartViewModel; implemented multipages.

---
+++ Cryptocurrency/ViewModel/StartViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using Cryptocurrency.Helper;
+using Cryptocurrency.Model.Data;
+using Cryptocurrency.Pages;
+using Cryptocurrency.Services.Implementation;
+using Cryptocurrency.Services.Interfaces;
+
+namespace Cryptocurrency.ViewModel
+{
+    public class StartViewModel : INotifyPropertyChanged
+    {
+        private readonly IRetrieveDataService _retrieveDataService;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ObservableCollection<Asset> Assets { get; set; } = new ObservableCollection<Asset>();
+        public Asset? SelectedAsset { get; set; }
+        public Page? CurrentPage { get; set; }
+
+        public StartViewModel(IRetrieveDataService retrieveDataService)
+        {
+            _retrieveDataService = retrieveDataService;
+            RetrieveAssets();
+        }
+
+        private async void RetrieveAssets()
+        {
+            var assets = await _retrieveDataService.GetAssetsAsync(1);
+            assets.ToList().ForEach(asset => Assets.Add(asset));
+        }
+    }
+}
