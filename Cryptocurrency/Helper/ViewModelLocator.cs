8467b4a32f853ed5f9ec30a255341497c397dd9f
MaksymTertychnyj <maka12008@ukr.net>
2022-07-05 13:18:12 +0300

    created ViewModelLocator

---
+++ Cryptocurrency/Helper/ViewModelLocator.cs
@@ -0,0 +1,22 @@
+﻿using Cryptocurrency.Services.Implementation;
+using Cryptocurrency.Services.Interfaces;
+using Cryptocurrency.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Cryptocurrency.Helper
+{
+    public class ViewModelLocator
+    {
+        private static IServiceProvider _provider;
+
+        public static void Init(IServiceProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public MainViewModel MainViewModel => _provider.GetRequiredService<MainViewModel>();
+        public StartViewModel StartViewModel => _provider.GetRequiredService<StartViewModel>();
+
+    }
+}
