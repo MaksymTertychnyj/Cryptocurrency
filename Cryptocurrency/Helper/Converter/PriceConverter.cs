54f8b1261b138dddb8f00170481e46397ae230a9
MaksymTertychnyj <maka12008@ukr.net>
2022-07-06 20:18:45 +0300

    added null checking to price converter

--- Cryptocurrency/Helper/Converter/PriceConverter.cs
+++ Cryptocurrency/Helper/Converter/PriceConverter.cs
@@ -12,16 +12,16 @@ namespace Cryptocurrency.Helper.Converter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            string stringPrice = value.ToString()!.Replace('.', ',');
+            if (value != null)
+            {
+                string stringPrice = value.ToString()!.Replace('.', ',');
 
-            if (value != null && double.TryParse(stringPrice, out double price))
-            {
-                return String.Format("{0:0.000}", price);
+                if (double.TryParse(stringPrice, out double price))
+                {
+                    return String.Format("{0:0.000}", price);
+                }
             }
-            else
-            {
-                return null!;
-            }
+            return null!;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
